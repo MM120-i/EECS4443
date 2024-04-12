f9a91de05ea228c7e46fea70a954d21d05981412
Mahim <mahimmarufuzzaman1234@gmail.com>
2024-04-12 04:51:46 -0400

    Kotlin Integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+buildscript {
+    val agp_version by extra("8.1.4")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0-Beta4" apply false
 }
